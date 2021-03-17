e6b1093cff1e557697e54143b2bab73be80c57bc
Sam Earle <smearle93@gmail.com>
2021-03-17 08:40:09 -0400

    Less invalid transactions.

--- data.py
+++ data.py
@@ -18,8 +18,11 @@ old_scene = scenes[0]
 with open('data/countries.json') as f:
    countries = json.load(f)
 
-#dists_df = pandas.read_csv('data/country_distances.csv', header=None)
-dists_df = pandas.read_csv('country-distance/output/distance-matrix.csv', header=0)
+def parse_name_str(str):
+    return str.replace(' ','_').replace(',','_').replace('(','_')
+
+dists_dict_df = pandas.read_csv('data/country_distances.csv', header=None)
+dists_df = pandas.read_csv('data/distance-matrix.csv', header=0)
 borders_df = pandas.read_csv('data/borders.csv')
 borders_df = borders_df.where(pandas.notnull(borders_df), None)
 
@@ -41,10 +44,18 @@ new_inventories = [[] for i in range(6)]
 
 items_inventory = []
 np.random.shuffle(items)
-items = items[:20]
+# In case we want to limit number of items in the game
+#items = items[:]
+
+items_inventory.append(
+    {'name': 'money',
+     'value': 100000000,
+     'hidden': False,}
+)
 
 for i in items:
 
+    i = parse_name_str(i)
     i_dict = {
         'name': i,
         'value': 0,
@@ -54,6 +65,11 @@ for i in items:
 
 old_inventories = old_game['inventories']
 new_inventories[0] = items_inventory
+new_inventories[1] = [
+    {'name': 'money',
+     'value': 1000000,
+     'hidden': False, }
+]
 #new_inventories[1] = old_inventories[1] + items_inventory
 assert len(new_inventories) == len(old_game['inventories'])
 new_game['inventories'] = new_inventories
@@ -77,24 +93,49 @@ new_scenes = []
 
 new_game['settings']['scrollSettings'] = {'defaultScrollSpeed': 0}
 
+country_nbs = {code: set() for code in countries.keys()}
+
 for code, c_name in countries.items():
-    travel_choices = []
     neighbors = \
         borders_df[borders_df['country_code'] == code]['country_border_code']
     neighbors = neighbors.where(pandas.notnull(neighbors), None)
-    neighbors = list(neighbors)
+    neighbors = set(neighbors)
     # if no land neighbors, provide some water routes
+
+    dist_neighbors = dists_df[dists_df['Unnamed: 0'] == code]
+    if dist_neighbors.shape[0] == 0:
+        dist_neighbors = dists_df[dists_df['Unnamed: 0'] == np.random.choice(iso_codes)]
+    dist_arr = dist_neighbors.to_numpy()[0, 1:]
+    dist_arr = sorted(list((dist, iso_codes[i]) for (i, dist) in enumerate(dist_arr)), key=lambda t: t[0])
+    dist_neighbors = [t[1] for t in dist_arr[:10]]
+    [neighbors.add(d) for d in dist_neighbors]
 
     if code not in iso_codes:
         print('Warning: no distances found for {}'.format(c_name))
     else:
-        span_nbs = span_df[span_df['Unnamed: 0'] == code].to_numpy()[0,1:]
+        span_nbs = span_df[span_df['Unnamed: 0'] == code].to_numpy()[0, 1:]
         idxs = span_nbs.nonzero()[0]
         for i in idxs:
             nb_code = iso_codes[i]
-            neighbors.append(nb_code)
+            neighbors.add(nb_code)
+
+        span_nbs_2 = span_df[code].to_numpy()
+        idxs = span_nbs.nonzero()[0]
+        for i in idxs:
+            nb_code = iso_codes[i]
+            neighbors.add(nb_code)
+    country_nbs[code] = neighbors
+    for nb_code in neighbors:
+        if nb_code in country_nbs:
+            country_nbs[nb_code].add(code)
+
+for code, c_name in countries.items():
+
+    added_nbs = set()
+    travel_choices = []
+    neighbors = country_nbs[code]
     for n in neighbors:
-        if n is None:
+        if n is None or n in added_nbs:
             continue
         if n not in countries:
             print('Error with {}'.format(n))
@@ -104,6 +145,7 @@ for code, c_name in countries.items():
             'text': 'Go to {}'.format(countries[n]),
             'nextScene': n,
         })
+        added_nbs.add(n)
 
 
    #if len(neighbors) ==1 and neighbors[0] is None or len(neighbors) == 0:
@@ -135,21 +177,54 @@ for code, c_name in countries.items():
 
     for x in exports.iloc:
         item = x['Item']
+        item_name = parse_name_str(item)
         quantity = x['Value']
         unit = x['Unit']
         flag = x['Flag Description']
+        ex_val = country_df[country_df['Item'] == item][country_df['Element'] == 'Export Value']
+        ex_price = ex_val['Value'].iloc[0]
+        price_unit = ex_val['Unit'].iloc[0]
         trade_choices.append({
             'name':
             item,
             'text':
-            'Buy 1 of {} {} of {}'.format(quantity, unit, item),
+            'Buy 1 {} for {} x {} (of {} {} exported)'.format(item, ex_price, price_unit, quantity, unit),
+            # TODO: track country inventories!
+            'nextScene': 'trade_{}'.format(code),
+#           'nextScene':
+#           code,
+            'addItem':
+            "{},1*(inv.money>0)".format(item_name),
+            'removeItem':
+                "money,{}".format(ex_price)
+        })
+
+    imports = country_df[country_df['Element'] == 'Import Quantity'].sort_values('Value')[-10:]
+
+
+    for i in imports.iloc:
+        item = i['Item']
+        item_name = parse_name_str(item)
+        quantity = i['Value']
+        unit = i['Unit']
+        flag = i['Flag Description']
+        im_val = country_df[country_df['Item'] == item][country_df['Element'] == 'Import Value']
+        im_price = im_val['Value'].iloc[0]
+        price_unit = im_val['Unit'].iloc[0]
+        trade_choices.append({
+            'name':
+                'sell_{}'.format(item),
+            'text':
+                'Sell 1 {} for {} x {}, (of {} {} imported)'.format(item, im_price, price_unit, quantity, unit),
             # TODO: track country inventories!
             #           'nextScene': 'trade_{}'.format(code),
-            'nextScene':
-            code,
+            'nextScene': 'trade_{}'.format(code),
+            'removeItem':
+                "{},1".format(item_name),
             'addItem':
-            "{},1".format(item)
+                "money,{}*inv.{},1".format(im_price, item_name)
         })
+
     trade_scene = copy.deepcopy(old_scene)
     trade_scene.update({
         'name': 'trade_{}'.format(code),
@@ -158,6 +233,7 @@ for code, c_name in countries.items():
     })
     new_scenes.append(trade_scene)
 
+np.random.shuffle(new_scenes)
 new_game['scenes'] = new_scenes
 
 with open('novel/novel.json', 'w') as f:
