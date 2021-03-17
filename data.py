bda48c1c2c582f420cf4ebbff9e5fbd45dc19b44
Sam Earle <smearle93@gmail.com>
2021-03-17 06:06:30 -0400

    Quasi-reasonable travel constraints! A spanning tree!!

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import copy
+from scipy.sparse.csgraph import minimum_spanning_tree
 import json
 from pdb import set_trace as T
 
@@ -12,10 +13,24 @@ with open('novel/novel_old.json') as f:
 scenes = old_game['scenes']
 old_scene = scenes[0]
 
-with open('data/countries.yaml') as f:
-    countries = yaml.load(f, yaml.Loader)
-# with open('data/countries.json') as f:
-#    countries = json.load(f)
+#with open('data/countries.yaml') as f:
+#    countries = yaml.load(f, yaml.Loader)
+with open('data/countries.json') as f:
+   countries = json.load(f)
+
+#dists_df = pandas.read_csv('data/country_distances.csv', header=None)
+dists_df = pandas.read_csv('country-distance/output/distance-matrix.csv', header=0)
+borders_df = pandas.read_csv('data/borders.csv')
+borders_df = borders_df.where(pandas.notnull(borders_df), None)
+
+adj_arr = dists_df.to_numpy()[:,1:].astype(np.float)
+adj_arr = minimum_spanning_tree(adj_arr).toarray()
+span_df = dists_df.copy()
+span_df.iloc[:, 1:] = adj_arr
+
+iso_codes = span_df.iloc[:, 0]
+iso_codes = iso_codes.where(pandas.notnull(iso_codes), None)
+iso_codes = list(span_df.iloc[:, 0])
 
 df = pandas.read_csv('data/trading.csv')
 items = np.array(list(df['Item']))
@@ -43,7 +58,11 @@ new_inventories[0] = items_inventory
 assert len(new_inventories) == len(old_game['inventories'])
 new_game['inventories'] = new_inventories
 
-countries = countries['countries']
+
+countries = {c['code']: c['name'] for c in countries}
+#countries = countries['countries']
+#countries['TL'] = 'East Timor'
+#countries['SS'] = 'South Sudan'
 
 choices = []
 
@@ -59,8 +78,40 @@ new_scenes = []
 new_game['settings']['scrollSettings'] = {'defaultScrollSpeed': 0}
 
 for code, c_name in countries.items():
-    travel_choices = copy.deepcopy(choices)
-    travel_choices= [{
+    travel_choices = []
+    neighbors = \
+        borders_df[borders_df['country_code'] == code]['country_border_code']
+    neighbors = neighbors.where(pandas.notnull(neighbors), None)
+    neighbors = list(neighbors)
+    # if no land neighbors, provide some water routes
+
+    if code not in iso_codes:
+        print('Warning: no distances found for {}'.format(c_name))
+    else:
+        span_nbs = span_df[span_df['Unnamed: 0'] == code].to_numpy()[0,1:]
+        idxs = span_nbs.nonzero()[0]
+        for i in idxs:
+            nb_code = iso_codes[i]
+            neighbors.append(nb_code)
+    for n in neighbors:
+        if n is None:
+            continue
+        if n not in countries:
+            print('Error with {}'.format(n))
+            continue
+        travel_choices.append({
+            'name': n,
+            'text': 'Go to {}'.format(countries[n]),
+            'nextScene': n,
+        })
+
+
+   #if len(neighbors) ==1 and neighbors[0] is None or len(neighbors) == 0:
+   #    neighbors = dists_df[dists_df[0] == code]
+    if len(neighbors) == 0:
+        print('Warning: no neighbors found for {}'.format(c_name))
+    #   travel_choices = copy.deepcopy(choices)
+    travel_choices = [{
         'name': 'trade_{}'.format(code),
         'text': 'Trade with {}'.format(c_name),
         'nextScene': 'trade_{}'.format(code),
@@ -80,22 +131,25 @@ for code, c_name in countries.items():
     }]
     country_df = df[df['Area'] == c_name]
     exports = country_df[country_df['Element'] ==
-            'Export Quantity'].sort_values('Value')[-10:]
+                         'Export Quantity'].sort_values('Value')[-10:]
+
     for x in exports.iloc:
         item = x['Item']
         quantity = x['Value']
         unit = x['Unit']
         flag = x['Flag Description']
-        trade_choices.append(
-            {
-                'name': item,
-                'text': 'Buy 1 of {} {} of {}'.format(
-                    quantity, unit, item),
-                #TODO: track country inventories!
-    #           'nextScene': 'trade_{}'.format(code),
-                'nextScene': code,
-                'addItem': "{},1".format(item)
-                })
+        trade_choices.append({
+            'name':
+            item,
+            'text':
+            'Buy 1 of {} {} of {}'.format(quantity, unit, item),
+            # TODO: track country inventories!
+            #           'nextScene': 'trade_{}'.format(code),
+            'nextScene':
+            code,
+            'addItem':
+            "{},1".format(item)
+        })
     trade_scene = copy.deepcopy(old_scene)
     trade_scene.update({
         'name': 'trade_{}'.format(code),
@@ -107,4 +161,5 @@ for code, c_name in countries.items():
 new_game['scenes'] = new_scenes
 
 with open('novel/novel.json', 'w') as f:
+
     json.dump(new_game, f, indent=4)
